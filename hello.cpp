98cb45870204eba323c08a2fdb9bc403b31ff1e7
longinglove <du_303412@163.com>
2016-05-24 23:31:52 +0800

    hello

---
+++ hello.cpp
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: hello.cpp
+	> Author: longing
+	> Mail: du_303412@163.com 
+	> Created Time: 2016年05月24日 星期二 23时30分08秒
+ ************************************************************************/
+
+#include<iostream>
+
+int main(int argc, char *argv[])
+{
+	std::cout<<"Hello, world\n";
+	return 0;
+}
+
